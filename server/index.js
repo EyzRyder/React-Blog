537218be386990d6b8121e6e007446682737dd29
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-12-24 20:29:40 -0300

    update security

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -6,7 +7,7 @@ const mongoose = require('mongoose');
 const blogRoutes = require('./routes/blogRoutes');
 const careerRoutes = require('./routes/careerRoutes');
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
@@ -16,7 +17,7 @@ app.use(morgan('dev'));
 
 mongoose.set("strictQuery", true);
 
-const dbURI = 'mongodb+srv://EyzRyder:fzo2URQ537@reactrouterblog.x3ekpou.mongodb.net/react-router-blog?retryWrites=true&w=majority';
+const dbURI = process.env.dbURI;
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => {
@@ -33,73 +34,3 @@ app.get('/', (req, res) => {
 
 app.use('/blogs', blogRoutes)
 app.use('/careers', careerRoutes)
-
-// app.post('/blogs', (req, res) => {
-//     const { title, body, author } = req.body;
-
-//     db.collection('blogs')
-//         .insertOne(
-//         {
-//             title: title,
-//             body: body,
-//             author: author
-//         }
-//     ).then(doc => {
-//         res.status(200).json(doc)
-//     })
-//         .catch(err => {
-//         res.status(500).json({ error: 'could not create new doc' })
-//     });
-// })
-
-
-// app.get('/blogs/:id', (req, res) => {
-
-//     if (ObjectId.isValid(req.params.id)) {
-//         db.collection('blogs')
-//             .findOne({ _id: new ObjectId(req.params.id) })
-//             .then(doc => {
-//                 res.status(200).json(doc)
-//             })
-//             .catch(() => {
-//                 res.status(500).json({ error: 'Could not fetch the documents' })
-//             })
-//     } else {
-//         res.status(500).json({ error: 'not a valid doc id' })
-//     }
-
-// });
-
-
-// app.delete('/blogs/:id', (req, res) => {
-//     if (ObjectId.isValid(req.params.id)) {
-//         db.collection('blogs')
-//             .deleteOne({ _id: new ObjectId(req.params.id) })
-//             .then(result => {
-//                 res.status(200).json(result)
-//             })
-//             .catch(() => {
-//                 res.status(500).json({ error: 'Could not delete the documents' })
-//             })
-//     } else {
-//         res.status(500).json({ error: 'not a valid doc id' })
-//     }
-// });
-
-// app.patch('/blogs/:id', (req, res) => {
-//     const updates = req.body;
-
-//     if (ObjectId.isValid(req.params.id)) {
-//         db.collection('books')
-//             .updateOne({ _id: new ObjectId(req.params.id) }, { $set: updates })
-//             .then(result => {
-//                 res.status(201).json(result);
-//             }).catch(err => {
-//                 res.status(500).json({ error: 'could not update new doc' })
-//             });
-//     } else {
-//         res.status(500).json({ error: 'not a valid doc id' })
-//     }
-
-
-// });
