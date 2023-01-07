ae474c86868b78cb98c8461e9a6700fc9c39a524
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-01-06 22:34:41 -0300

    update about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
 const About = () => {
+    const [user, setUser] = useState("bessi")
+    
+    if (!user) {
+        return <Navigate to='/' replace={true}/>
+    }
     return (
         <div>
             <h2 className="text-3xl text-primary-100 mb-8">Sobre</h2>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste quasi illum dignissimos nostrum voluptatum optio commodi vel odit ipsa necessitatibus. Deleniti amet ea minima eum repellendus sint rem quos esse.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste quasi illum dignissimos nostrum voluptatum optio commodi vel odit ipsa necessitatibus. Deleniti amet ea minima eum repellendus sint rem quos esse.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste quasi illum dignissimos nostrum voluptatum optio commodi vel odit ipsa necessitatibus. </p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste quasi illum dignissimos nostrum voluptatum optio commodi vel odit ipsa necessitatibus. Deleniti amet ea minima eum repellendus sint rem quos esse.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste quasi illum dignissimos nostrum voluptatum optio commodi vel odit ipsa necessitatibus. Deleniti amet ea minima eum repellendus sint rem quos esse.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste quasi illum dignissimos nostrum voluptatum optio commodi vel odit ipsa necessitatibus. </p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste quasi illum dignissimos nostrum voluptatum optio commodi vel odit ipsa necessitatibus. Deleniti amet ea minima eum repellendus sint rem quos esse.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste quasi illum dignissimos nostrum voluptatum optio commodi vel odit ipsa necessitatibus. Deleniti amet ea minima eum repellendus sint rem quos esse.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste quasi illum dignissimos nostrum voluptatum optio commodi vel odit ipsa necessitatibus. </p>
+
+            <button onClick={() => setUser(null)}>Logout</button>
+
         </div>
     );
 }
